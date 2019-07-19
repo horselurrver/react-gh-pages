4e11bd12704a633ae5deb3206539daacb07adb26
Amy Wang <horselurrver@gmail.com>
2019-07-19 13:02:54 -0400

    Add config

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const tokenUrl = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/0e96fcde-6705-4b06-8703-1fb324bca30a/token';
+const instanceLocator = 'v1:us1:0e96fcde-6705-4b06-8703-1fb324bca30a';
+
+export { tokenUrl, instanceLocator }
